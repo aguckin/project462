0a5d129702c50b1f201a11fb168a55956e87b1c3
Alex Guckin <amg6612@rit.edu>
2017-11-03 19:31:16 -0400

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # encryptoviz-CSEC462-2171-guckin-wegryn
+#added json module
+#added nmap module
--- engine.py
+++ engine.py
@@ -4,10 +4,13 @@
 #this way i can figure out how much encryption is on the network.  
 
 import nmap #need to install python-nmap via PIP
+import json # need to learn json 
+
 
 nm = nmap.PortScanner()
-nm.scan('10.0.0.1', '22') #testing functionality
-nm.command_line() 'nmap -n -p 22-sV 10.0.0.1'
+nm.scan('10.21.0.6/32', '53') #testing functionality
+print json.dumps({'4': 5, '6': 7}, indent=2,  separators=(',',':'))
+
 
 print(nm.scaninfo())
 print(nm.all_hosts())
